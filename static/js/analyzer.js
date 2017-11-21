8f4d0dcae4e7482b6f12de13fb650a0d74028e64
jayrsawal <jayrsawal@gmail.com>
2017-11-21 16:27:29 -0500

    Add and remove controls for search terms

--- static/js/analyzer.js
+++ static/js/analyzer.js
@@ -1,8 +1,9 @@
 var searching = true, peeking = false;
 var target = "";
+var target_parent = "";
 var target_index = "";
 
-$("svg").click(function() {
+$(document).on("click", ".term-vis svg", function() {
     var $parent = $(this).parent().parent();
     $(".term-container").removeClass("selected");
     $parent.addClass("selected");
@@ -11,10 +12,10 @@ $("svg").click(function() {
         toggleSearchDialog();
     }
     target = $(this).attr("id");
+    target_parent = $(this).parent().attr("id");
     target_index = $(this).attr("tier-index");
     var vis_height = $(window).height()-$("#search-term-container").height()-80;
     var heading_id = $(".term-heading-id", $parent).val();
-    
     createNewVis("#search-dialog", "search-dialog-vis", "/oht/"
         , target_index, $("#search-dialog").width()-2, vis_height, 1
         , true, true, true, keywordClicked, heading_id);
@@ -27,8 +28,22 @@ $("#search-result-container").on("click", function() {
         togglePeek();    
     }
 });
+
 $("#search-dialog").on("click", function() {
     if(peeking) togglePeek();
+});
+
+$("#add-term").on("click", function() {
+    drawSearchTerm("1.NA.NA.NA.NA.NA.NA.1", 181456, "The world", 0);
+    $("#search-term-box").sortable("destroy");
+    $("#search-term-box").sortable({
+        items: ".term-container"
+    });
+});
+
+$(window).on("resize", function() {
+    searching = true;
+    toggleSearchDialog();
 });
 
 $(document).keyup(function(e) {
@@ -50,15 +65,24 @@ $(document).ready(function() {
 function toggleSearchDialog() {
     if(searching) {
         $("#search-dialog").css("left", -$(window).width()-75);
-        $("#search-result-container").css("left", 0);
-        $("#search-result-container").css("width", "100vw");
+        $("#search-result-container").css({
+            "left": 0,
+            "width": "100vw",
+            "padding-left": "2%",
+            "padding-right": "2%"
+        });
+        $("#search-dialog svg").css("padding-left", 0);
         searching = false;
         peeking = false;
         $(".term-container").removeClass("selected");    
     } else {
         $("#search-dialog").css("left", 0);
-        $("#search-result-container").css("left", $("#search-dialog").width());
-        $("#search-result-container").css("width", "30vw");
+        $("#search-result-container").css({
+            "left": $("#search-dialog").width(),
+            "width": "30vw",
+            "padding-left": "0",
+            "padding-right": "0"
+        });
         searching = true;
     }
 }
@@ -69,10 +93,12 @@ function togglePeek() {
         var search_offset = ($("#search-dialog").width()+$("#search-result-container").width()) - $(window).width();
         if(peeking) {
             $("#search-dialog").css("left", 0);
+            $("#search-dialog svg").css("padding-left", 0);
             $("#search-result-container").css("left", $("#search-dialog").width());
             peeking = false;
         } else {
             $("#search-dialog").css("left", -(search_offset));
+            $("#search-dialog svg").css("padding-left", search_offset/2);
             $("#search-result-container").css("left", result_offset);
             peeking = true;
         }
@@ -81,25 +107,22 @@ function togglePeek() {
 
 function keywordClicked(d) {
     var new_tier_index = d.data.parent;
-    var $target = $("#"+target);
+    var $target = $("#"+target, "#"+target_parent);
     var weight = parseFloat($target.attr("weight"));
     var vis_size = min_size + (add_size * weight);
 
     $(".term-heading-id", $target.parent().parent()).val(d.data.heading_id);
     $(".term-heading", $target.parent().parent()).html(d.data.name);
-
-    createNewVis("#search-term-box", target, "/oht/"
-        , new_tier_index, vis_size, vis_size, weight, false, false, false);
+    createNewVis("#search-term-box #"+target_parent, target, "/oht/"
+        , new_tier_index, vis_size, vis_size, weight, false, false, false, null, d.data.heading_id);
     $target.attr("tier-index", new_tier_index);
     toggleSearchDialog();
 
-    d3.select("#"+target)
-        .transition().duration(0)
-            .style("background", "white")
-        .transition().duration(500)
-            .style("background", "#a8d1ff")
-        .transition().delay(500).duration(500)
-            .style("background", "white");
+    $("#"+target_parent).parent().removeClass("selected");        
+    $("#"+target_parent).parent().addClass("changed");
+    setTimeout(function() {
+        $("#"+target_parent).parent().removeClass("changed");
+    }, 500);
 }
 
 function search() {
--- static/js/explore.js
+++ static/js/explore.js
@@ -29,9 +29,7 @@ function hovered(hover) {
 function clicked(cb_keyword) {
     return function(d) {
         if( d.data.keyword ) {
-            if(typeof(cb_keyword) != "undefined") {
-                cb_keyword(d);
-            }
+            cb_keyword(d);
             return;
         }
         var $container = $(this).closest("svg");
@@ -46,8 +44,9 @@ function clicked(cb_keyword) {
 function createNewVis(svg_path, svg_id, path, id, width, height, weight, change_focus=true, add_label=true, add_event=true, cb_keyword, heading_id) {
     svg_id = svg_id.replace(/\./g, "-");
     var svg = null;
-    if($("#"+svg_id).length > 0) {
-        svg = d3.select("#"+svg_id);
+
+    if($(svg_path + " #"+svg_id).length > 0) {
+        svg = d3.select(svg_path + " #"+svg_id);
     } else {
         svg = d3.select(svg_path).append("svg");
         svg.append("g").attr("transform", "translate(2.5,2.5)");
@@ -60,15 +59,10 @@ function createNewVis(svg_path, svg_id, path, id, width, height, weight, change_
     svg.attr("tier-index", id);
     
     var pack = d3.pack().size([width-5, height-5]).padding(padding);
-
     update(svg, pack, path, id, change_focus, add_label, add_event, heading_id, cb_keyword);
 }
 
 function update(svg, pack, path, id, change_focus=true, add_label=true, add_event=true, heading_id, cb_keyword) {
-    if(change_focus && focus_id == id) {
-        return;
-    }
-
     var url_path = path + id;
     d3.csv(url_path, function(error, data) {
         if (error) throw error;
@@ -106,7 +100,7 @@ function update(svg, pack, path, id, change_focus=true, add_label=true, add_even
         .attr("r", function(d) { return d.r; })
         .style("fill", function(d) { 
             if(d.data.keyword) {
-                if(d.data.heading_id == heading_id) {
+                if(d.data.heading_id == heading_id) {                    
                     return "rgb(255,0,0)";
                 } else {
                     return color(-4);
@@ -152,14 +146,22 @@ function collapse(d) {
 
 function drawSearchTerm(tier_index, heading_id, heading_text, weight) {
     tier_id = tier_index.replace(/\./g, "-");
-    var $box = $("<div class='term-container text-center' id='term-"
-        + tier_id + "'><input type='hidden' class='term-heading-id' value='" 
+    var container_path = "term-" + tier_id;
+    container_path += "-" + $("[id^=" + container_path + "]").length.toString();
+    var svg_path = "term-vis-" + tier_id;
+    svg_path += "-" + $("[id^=" + svg_path + "]").length.toString();
+    var $box = $("<div class='term-container text-center' id='"
+        + container_path + "'><button class='close' onclick='$(this).parent().remove();'>\
+        <span>&times;</span></button><input type='hidden' class='term-heading-id' value='" 
         + heading_id +"'/><div class='term-heading'>"
-        + heading_text + "</div><div class='term-vis' id='term-vis-" 
-        + tier_id + "'></div></div>");
+        + heading_text + "</div><div class='term-vis' id='" 
+        + svg_path + "'></div></div>");
     
-    $("#search-term-box").append($box);
+    $("#add-term").before($box);
     var vis_size = min_size + (add_size * weight);
-    createNewVis("#term-vis-" + tier_id, "mini-" + tier_id
+    
+    svg_path = "#" + svg_path;
+
+    createNewVis(svg_path, "mini-" + tier_id
         , "/oht/", tier_index, vis_size, vis_size, weight, false, false, false, null, heading_id);
 }
